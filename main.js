ae9923a2ca256dc1dc71c81ecafa930d5cab6ea9
Daniel Souza <92832521+danielsouza04@users.noreply.github.com>
2021-10-23 23:28:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const LinksSocialMedia = {
+  github: 'danielsouza04',
+  youtube: 'UCnu2wr738xx3JV-pn97ChuQ',
+  instagram: 'danielhs2004',
+  facebook: 'profile.php?id=100006426674242',
+  twitter: 'BiGoDoNiS1'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+    alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
